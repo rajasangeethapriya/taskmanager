fc2cb26241ff8e471b198d396405bfdfd2a650a5
Your Name <you@example.com>
2019-08-19 14:17:11 +0530

    Checking in UI Layer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
 
+
+
+import {MatSliderModule} from '@angular/material/slider'; 
+import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+   declarations: [
+      AppComponent
+   ],
+   imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      MatTabsModule,
+      FormsModule,
+      ReactiveFormsModule,
+      FlexLayoutModule,
+      MatSliderModule,
+      MatCardModule,
+      MatInputModule,MatDatepickerModule,MatGridListModule
+      
+   ],
+   exports:[
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    BrowserAnimationsModule,
+    MatTabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatSliderModule,
+    MatCardModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
